28ace95fe1b7b45c52e6d93e0711b9501516bf48
Anton Maiseyeu <anet256@mail.ru>
2021-03-06 13:09:18 +0300

    Fixed bug with extra import. Updated API addresses.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import NavBar from "./Navbar";
 import Footer from "./Footer";
 
 function Layout(props) {
--- src/store/thunks.js
+++ src/store/thunks.js
@@ -2,30 +2,30 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const getHeroes = createAsyncThunk("heroes", async () => {
-  const { data } = await axios.get(`http://localhost:5000/auth/heroAdd`);
+  const { data } = await axios.get(`https://mogilev-library-remember.azurewebsites.net/auth/heroAdd`);
   return data;
 });
 
 
 export const getHero = createAsyncThunk("hero", async ({id}) => {
-  const { data } = await axios.get(`http://localhost:5000/heroes/hero/?id=${id}`);
+  const { data } = await axios.get(`https://mogilev-library-remember.azurewebsites.net/heroes/hero/?id=${id}`);
   return data;
 });
 
 export const getHeroByName = createAsyncThunk("hero", async ({name}) => {
-  const { data } = await axios.get(`http://localhost:5000/heroes/search?lastName=${name}`);
+  const { data } = await axios.get(`https://mogilev-library-remember.azurewebsites.net/search?lastName=${name}`);
   return data;
 });
 
 export const getHistory = createAsyncThunk("history", async () => {
-  const { data } = await axios.get(`http://localhost:5000/history`);
+  const { data } = await axios.get(`https://mogilev-library-remember.azurewebsites.net/history`);
   return data;
 });
 export const getHistoryByDate = createAsyncThunk("historyBy", async ({id}) => {
-  const { data } = await axios.get(`http://localhost:5000/history?id=${id}`);
+  const { data } = await axios.get(`https://mogilev-library-remember.azurewebsites.net/history?id=${id}`);
   return data;
 });
 export const createNewHero = createAsyncThunk('heroUser', async (hero)=>{
-  const {data} = await axios.post('http://localhost:5000/auth/heroAdd',hero)
+  const {data} = await axios.post('https://mogilev-library-remember.azurewebsites.net/auth/heroAdd',hero)
   return data
 })
