6e5f2540afc53edea2eba4591f90c075c68e5b50
A.Akhmayeva <A.Akhmayeva@itechart-group.com>
2021-05-04 16:30:19 +0300

    feat: update icons file, add image for edit

---
+++ src/assets/icons/index.js
@@ -0,0 +1,12 @@
+import Admin from './user-lock-solid.svg';
+import Vk from './vk-square.svg';
+import Facebook from './facebook-square-brands.svg';
+import Ok from './odnoklassniki-square-brands.svg';
+import Google from './google-plus-square-brands.svg';
+import Close from './close.svg';
+import Upload from './upload-solid.svg';
+import leftArrow from './chevron-left-solid.svg';
+import rightArrow from './chevron-right-solid.svg';
+import Arrow from './arrow-right-solid.svg';
+
+export { Admin, Upload, Vk, Ok, Google, Close, Facebook, leftArrow, rightArrow, Arrow };
