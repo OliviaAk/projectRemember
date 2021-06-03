7aafbbace31cd277d0615321d24bd809c4921ddd
A.Akhmayeva <A.Akhmayeva@itechart-group.com>
2021-06-03 12:31:14 +0300

    remove

--- src/sum.test.js
+++ src/sum.test.js
@@ -11,7 +11,3 @@ test('adds 1 + 3 to equal 4', () => {
 test('adds 1 + 3 to equal 4', () => {
   expect(sum(1, 6)).toBe(7);
 });
-
-test('adds 1 + 3 to equal 4', () => {
-  expect(sum(0, 3)).toBe(0);
-});
